e8595ac449968a0455e3237638ae4a3d20d40eec
shrekt444 <andrea.maie444@gmail.com>
2021-05-13 11:11:44 +0200

    vue completo

---
+++ src/app.js
@@ -0,0 +1,22 @@
+var app = new Vue({
+    el: '#root',
+    data: {
+        options: [
+            'Home',
+            'Pages',
+            'Portfolio',
+            'Blog',
+            'Shop',
+            'Elements',
+        ],
+        contacts: [
+            'maree.qode@gmail.com',
+            '+44645321789'
+        ],
+        adresses: [
+            'Avenue d Auderghem 10',
+            '1040 Brussels, Belgium'
+        ]
+    },
+    methods: {}
+})
